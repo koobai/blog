08d30638fa886a619a3791dd28571e6f5e11191a
koobai <50470569+koobai@users.noreply.github.com>
2023-03-18 19:54:18 +0800

    add

--- themes/jingzhe/assets/js/scripts.js
+++ themes/jingzhe/assets/js/scripts.js
@@ -10,3 +10,26 @@ if (menuTrigger !== null) {
     body.classList.toggle('lock-scroll');
   });
 }
+// 回到顶部
+window.onscroll = function() {scrollTopButton()};
+
+function scrollTopButton() {
+  if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+    document.getElementById("gotop").style.display = "block";
+  } else {
+    document.getElementById("gotop").style.display = "none";
+  }
+}
+function smoothScrollTop(){
+    var timer  = null;
+    cancelAnimationFrame(timer);
+    timer = requestAnimationFrame(function fn(){
+        var oTop = document.body.scrollTop || document.documentElement.scrollTop;
+        if(oTop > 0){
+            document.body.scrollTop = document.documentElement.scrollTop = oTop - 150;
+            timer = requestAnimationFrame(fn);
+        }else{
+            cancelAnimationFrame(timer);
+        }
+    });
+}
