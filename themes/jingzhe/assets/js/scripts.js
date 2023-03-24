5042ee52a1016a98f2718f7a5683a231b70d3ae7
koobai <50470569+koobai@users.noreply.github.com>
2023-03-24 14:56:46 +0800

    add

--- themes/jingzhe/assets/js/scripts.js
+++ themes/jingzhe/assets/js/scripts.js
@@ -11,53 +11,10 @@ if (menuTrigger !== null) {
   });
 }
 // 回到顶部
-window.onscroll = function() {scrollTopButton()};
+window.onscroll=function(){(document.body.scrollTop>500||document.documentElement.scrollTop>500)?document.getElementById("gotop").style.display="block":document.getElementById("gotop").style.display="none"};function smoothScrollTop(){var e=null;cancelAnimationFrame(e),e=requestAnimationFrame(function n(){var o=document.body.scrollTop||document.documentElement.scrollTop;o>0?(document.body.scrollTop=document.documentElement.scrollTop=o-150,e=requestAnimationFrame(n)):cancelAnimationFrame(e)})}
 
-function scrollTopButton() {
-  if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
-    document.getElementById("gotop").style.display = "block";
-  } else {
-    document.getElementById("gotop").style.display = "none";
-  }
-}
-function smoothScrollTop(){
-    var timer  = null;
-    cancelAnimationFrame(timer);
-    timer = requestAnimationFrame(function fn(){
-        var oTop = document.body.scrollTop || document.documentElement.scrollTop;
-        if(oTop > 0){
-            document.body.scrollTop = document.documentElement.scrollTop = oTop - 150;
-            timer = requestAnimationFrame(fn);
-        }else{
-            cancelAnimationFrame(timer);
-        }
-    });
-}
 // 灯箱调用(首页顶部/Memos页面)
 window.ViewImage && ViewImage.init('.photo-home a, .datacont img, .content_zhengwen img, .top-img');
 
-const nav = document.querySelector('.header-background');
-let lastScrollTop = 0;
-let initialScrollTop = window.scrollY;
-let scrollUpDistance = 0;
-const SCROLL_DISTANCE_DOWN = 200; // 向下滑动距离
-const SCROLL_DISTANCE_UP = 100; // 向上滑动距离
-
-window.addEventListener('scroll', () => {
-  const scrollTop = window.scrollY;
-
-  if (scrollTop > lastScrollTop && scrollTop > SCROLL_DISTANCE_DOWN) {
-    nav.classList.add('hidden');
-  } else if (scrollTop < lastScrollTop) {
-    scrollUpDistance += lastScrollTop - scrollTop;
-    if (scrollUpDistance > SCROLL_DISTANCE_UP) {
-      nav.classList.remove('hidden');
-      scrollUpDistance = 0;
-    }
-  } else {
-    scrollUpDistance = 0;
-    nav.classList.remove('hidden');
-  }
-
-  lastScrollTop = scrollTop;
-});
+// 导航自动隐藏及显示
+const e=document.querySelector(".header-background"),t=window.scrollY,a=200,n=100;let o=0,r=t;window.addEventListener("scroll",()=>{const t=window.scrollY;t>r&&t>a?e.classList.add("hidden"):t<r&&(o+=r-t,o>n&&(e.classList.remove("hidden"),o=0)),r=t})
