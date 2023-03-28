8de222e174ac376c414805ae22720232daeb5e32
koobai <50470569+koobai@users.noreply.github.com>
2023-03-28 16:29:54 +0800

    add

--- static/js/memos.js
+++ static/js/memos.js
@@ -1,222 +1,149 @@
 /*
-Last Modified time : 20230224 13:00 by https://immmmm.com
+Last Modified time : 20230326 13:00 by https://immmmm.
 最新js https://immmmm.com/bb-lmm-mk.js
 */
 var bbMemo = {
-    memos: 'https://memos.koobai.com/',
-    limit: '10',
-    creatorId: '1',
-    domId: '#bber',
+  memos: 'https://demo.usememos.com/',
+  limit: '10',
+  creatorId: '101',
+  domId: '#bber',
 }
 if(typeof(bbMemos) !=="undefined"){
-    for(var key in bbMemos) {
-      if(bbMemos[key]){
-        bbMemo[key] = bbMemos[key];
-      }
+  for(var key in bbMemos) {
+    if(bbMemos[key]){
+      bbMemo[key] = bbMemos[key];
     }
+  }
 }
 function loadCssCode(code){
-  var style = document.createElement('style');
-  style.type = 'text/css';
-  style.rel = 'stylesheet';
-  //for Chrome Firefox Opera Safari
-  style.appendChild(document.createTextNode(code));
-  //for IE
-  //style.styleSheet.cssText = code;
-  var head = document.getElementsByTagName('head')[0];
-  head.appendChild(style);
+var style = document.createElement('style');
+style.type = 'text/css';
+style.rel = 'stylesheet';
+style.appendChild(document.createTextNode(code));
+var head = document.getElementsByTagName('head')[0];
+head.appendChild(style);
 }
-var allCSS ='';
-loadCssCode(allCSS);
 
 var limit = bbMemo.limit
 var memos = bbMemo.memos
 var page = 1,offset = 0,nextLength = 0,nextDom='';
 var bbDom = document.querySelector(bbMemo.domId);
-var load = '<div class="bb-load"><button class="load-btn button-load">努力加载中...，如遇等待时间过长请刷新页面</button></div>'
+var load = '<div class="bb-load"><button class="load-btn button-load">加载中……</button></div>'
 if(bbDom){
-  getFirstList() //首次加载数据
-  meNums() //加载总数
-  var btn = document.querySelector("button.button-load");
-  btn.addEventListener("click", function () {
-    btn.textContent= '加努力加载中...，如遇等待时间过长请刷新页面';
-    updateHTMl(nextDom)
-    if(nextLength < limit){ //返回数据条数小于限制条数，隐藏
-      document.querySelector("button.button-load").remove()
-      return
-    }
-    getNextList()
-  });
+getFirstList() //首次加载数据
+meNums() //加载总数
+var btn = document.querySelector("button.button-load");
+btn.addEventListener("click", function () {
+  btn.textContent= '加载中……';
+  updateHTMl(nextDom)
+  if(nextLength < limit){ //返回数据条数小于限制条数，隐藏
+    document.querySelector("button.button-load").remove()
+    return
+  }
+  getNextList()
+});
 }
 function getFirstList(){
-  bbDom.insertAdjacentHTML('afterend', load);
-  var bbUrl = memos+"api/memo?creatorId="+bbMemo.creatorId+"&rowStatus=NORMAL&limit="+limit;
-  fetch(bbUrl).then(res => res.json()).then( resdata =>{
-    updateHTMl(resdata.data)
-    var nowLength = resdata.data.length
-    if(nowLength < limit){ //返回数据条数小于 limit 则直接移除“加载更多”按钮，中断预加载
-      document.querySelector("button.button-load").remove()
-      return
-    }
-    page++
-    offset = limit*(page-1)
-    getNextList()
-  });
+bbDom.insertAdjacentHTML('afterend', load);
+var bbUrl = memos+"api/memo?creatorId="+bbMemo.creatorId+"&rowStatus=NORMAL&limit="+limit;
+fetch(bbUrl).then(res => res.json()).then( resdata =>{
+  updateHTMl(resdata.data)
+  var nowLength = resdata.data.length
+  if(nowLength < limit){ //返回数据条数小于 limit 则直接移除“加载更多”按钮，中断预加载
+    document.querySelector("button.button-load").remove()
+    return
+  }
+  page++
+  offset = limit*(page-1)
+  getNextList()
+});
 }
 //预加载下一页数据
 function getNextList(){
-  var bbUrl = memos+"api/memo?creatorId="+bbMemo.creatorId+"&rowStatus=NORMAL&limit="+limit+"&offset="+offset;
-  fetch(bbUrl).then(res => res.json()).then( resdata =>{
-    nextDom = resdata.data
-    nextLength = nextDom.length
-    page++
-    offset = limit*(page-1)
-    if(nextLength < 1){ //返回数据条数为 0 ，隐藏
-      document.querySelector("button.button-load").remove()
-      return
-    }
-  })
+var bbUrl = memos+"api/memo?creatorId="+bbMemo.creatorId+"&rowStatus=NORMAL&limit="+limit+"&offset="+offset;
+fetch(bbUrl).then(res => res.json()).then( resdata =>{
+  nextDom = resdata.data
+  nextLength = nextDom.length
+  page++
+  offset = limit*(page-1)
+  if(nextLength < 1){ //返回数据条数为 0 ，隐藏
+    document.querySelector("button.button-load").remove()
+    return
+  }
+})
 }
 //加载总 Memos 数
 function meNums(){
-  var bbLoad = document.querySelector('.bb-load')
-  var bbUrl = memos+"api/memo/stats?creatorId="+bbMemo.creatorId
-  fetch(bbUrl).then(res => res.json()).then( resdata =>{
-    if(resdata.data){
-      var allnums = ''
-      bbLoad.insertAdjacentHTML('afterend', allnums);
-    }
-  })
+var bbLoad = document.querySelector('.bb-load')
+var bbUrl = memos+"api/memo/stats?creatorId="+bbMemo.creatorId
+fetch(bbUrl).then(res => res.json()).then( resdata =>{
+  if(resdata.data){
+    var allnums = ' ( 目前共唠叨了 '+resdata.data.length+' 条 )'
+    bbLoad.insertAdjacentHTML('afterend', allnums);
+  }
+})
 }
-
 // 插入 html 
 function updateHTMl(data){
-  var result="",resultAll="";
-  const TAG_REG = /#([^\s#]+?) /g
-  , BILIBILI_REG = /<a.*?href="https:\/\/www\.bilibili\.com\/video\/((av[\d]{1,10})|(BV([\w]{10})))\/?".*?>.*<\/a>/g
-  , NETEASE_MUSIC_REG = /<a.*?href="https:\/\/music\.163\.com\/.*id=([0-9]+)".*?>.*<\/a>/g
-  , QQMUSIC_REG = /<a.*?href="https\:\/\/y\.qq\.com\/.*(\/[0-9a-zA-Z]+)(\.html)?".*?>.*?<\/a>/g
-  , QQVIDEO_REG = /<a.*?href="https:\/\/v\.qq\.com\/.*\/([a-z|A-Z|0-9]+)\.html".*?>.*<\/a>/g
-  , YOUKU_REG = /<a.*?href="https:\/\/v\.youku\.com\/.*\/id_([a-z|A-Z|0-9|==]+)\.html".*?>.*<\/a>/g
-  , YOUTUBE_REG = /<a.*?href="https:\/\/www\.youtube\.com\/watch\?v\=([a-z|A-Z|0-9]{11})\".*?>.*<\/a>/g;
-  marked.setOptions({
-    breaks: !0,
-    smartypants: !0,
-    langPrefix: 'language-'
-  });
-  for(var i=0;i < data.length;i++){
-      var bbContREG = data[i].content
-        .replace(TAG_REG, "<span class='tag-span'>#$1</span> ")
-
-      bbContREG = marked.parse(bbContREG)
-        .replace(BILIBILI_REG, "<div class='video-wrapper'><iframe src='//player.bilibili.com/player.html?bvid=$1&as_wide=1&high_quality=1&danmaku=0' scrolling='no' border='0' frameborder='no' framespacing='0' allowfullscreen='true'></iframe></div>")
-        .replace(NETEASE_MUSIC_REG, "<meting-js auto='https://music.163.com/#/song?id=$1'></meting-js>")
-        .replace(QQMUSIC_REG, "<meting-js auto='https://y.qq.com/n/yqq/song$1.html'></meting-js>")
-        .replace(QQVIDEO_REG, "<div class='video-wrapper'><iframe src='//v.qq.com/iframe/player.html?vid=$1' allowFullScreen='true' frameborder='no'></iframe></div>")
-        .replace(YOUKU_REG, "<div class='video-wrapper'><iframe src='https://player.youku.com/embed/$1' frameborder=0 'allowfullscreen'></iframe></div>")
-        .replace(YOUTUBE_REG, "<div class='video-wrapper'><iframe src='https://www.youtube.com/embed/$1' title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowfullscreen title='YouTube Video'></iframe></div>")
-        
-      //console.log(bbContREG)
-      //解析内置资源文件
-      if(data[i].resourceList && data[i].resourceList.length > 0){
-        var resourceList = data[i].resourceList;
-        var imgUrl='',resUrl='',resImgLength = 0;
-        for(var j=0;j < resourceList.length;j++){
-          var restype = resourceList[j].type.slice(0,5)
-          var resexlink = resourceList[j].externalLink
-          var resLink = ''
-          if(resexlink){
-            resLink = resexlink
-          }else{
-            resLink = memos+'o/r/'+resourceList[j].id+'/'+resourceList[j].filename
-          }
-          if(restype == 'image'){
-            imgUrl += '<figure class="gallery-thumbnail"><img loading="lazy" decoding="async" class="img thumbnail-image" src="'+resLink+'"/></figure>'
-            resImgLength = resImgLength + 1 
-          }
-          if(restype !== 'image'){
-            resUrl += '<a target="_blank" rel="noreferrer" href="'+resLink+'">'+resourceList[j].filename+'</a>'
-          }
+var result="",resultAll="";
+const TAG_REG = /#([^\s#]+?) /g
+, BILIBILI_REG = /<a.*?href="https:\/\/www\.bilibili\.com\/video\/((av[\d]{1,10})|(BV([\w]{10})))\/?".*?>.*<\/a>/g
+, NETEASE_MUSIC_REG = /<a.*?href="https:\/\/music\.163\.com\/.*id=([0-9]+)".*?>.*<\/a>/g
+, QQMUSIC_REG = /<a.*?href="https\:\/\/y\.qq\.com\/.*(\/[0-9a-zA-Z]+)(\.html)?".*?>.*?<\/a>/g
+, QQVIDEO_REG = /<a.*?href="https:\/\/v\.qq\.com\/.*\/([a-z|A-Z|0-9]+)\.html".*?>.*<\/a>/g
+, YOUKU_REG = /<a.*?href="https:\/\/v\.youku\.com\/.*\/id_([a-z|A-Z|0-9|==]+)\.html".*?>.*<\/a>/g
+, YOUTUBE_REG = /<a.*?href="https:\/\/www\.youtube\.com\/watch\?v\=([a-z|A-Z|0-9]{11})\".*?>.*<\/a>/g;
+marked.setOptions({
+  breaks: !0,
+  smartypants: !0,
+  langPrefix: 'language-'
+});
+for(var i=0;i < data.length;i++){
+    var bbContREG = data[i].content.replace(TAG_REG, "<span class='tag-span'>#$1</span> ")
+    bbContREG = marked.parse(bbContREG)
+      .replace(BILIBILI_REG, "<div class='video-wrapper'><iframe src='//player.bilibili.com/player.html?bvid=$1&as_wide=1&high_quality=1&danmaku=0' scrolling='no' border='0' frameborder='no' framespacing='0' allowfullscreen='true'></iframe></div>")
+      .replace(NETEASE_MUSIC_REG, "<meting-js auto='https://music.163.com/#/song?id=$1'></meting-js>")
+      .replace(QQMUSIC_REG, "<meting-js auto='https://y.qq.com/n/yqq/song$1.html'></meting-js>")
+      .replace(QQVIDEO_REG, "<div class='video-wrapper'><iframe src='//v.qq.com/iframe/player.html?vid=$1' allowFullScreen='true' frameborder='no'></iframe></div>")
+      .replace(YOUKU_REG, "<div class='video-wrapper'><iframe src='https://player.youku.com/embed/$1' frameborder=0 'allowfullscreen'></iframe></div>")
+      .replace(YOUTUBE_REG, "<div class='video-wrapper'><iframe src='https://www.youtube.com/embed/$1' title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowfullscreen title='YouTube Video'></iframe></div>")
+    
+    //解析内置资源文件
+    if(data[i].resourceList && data[i].resourceList.length > 0){
+      var resourceList = data[i].resourceList;
+      var imgUrl='',resUrl='',resImgLength = 0;
+      for(var j=0;j < resourceList.length;j++){
+        var restype = resourceList[j].type.slice(0,5)
+        var resexlink = resourceList[j].externalLink
+        var resLink = ''
+        if(resexlink){
+          resLink = resexlink
+        }else{
+          resLink = memos+'o/r/'+resourceList[j].id+'/'+resourceList[j].filename
         }
-        if(imgUrl){
-          var resImgGrid = ""
-          if(resImgLength !== 1){var resImgGrid = "grid grid-"+resImgLength}
-          bbContREG += '<div class="resimg '+resImgGrid+'">'+imgUrl+'</div></div>'
+        if(restype == 'image'){
+          imgUrl += '<figure class="gallery-thumbnail"><img class="img thumbnail-image" src="'+resLink+'"/></figure>'
+          resImgLength = resImgLength + 1 
         }
-        if(resUrl){
-          bbContREG += '<p class="datasource">'+resUrl+'</p>'
+        if(restype !== 'image'){
+          resUrl += '<a target="_blank" rel="noreferrer" href="'+resLink+'">'+resourceList[j].filename+'</a>'
         }
       }
-      result += "<li class='bb-list-li' data-module='fade-in-up'><span class='datatime'>"+ moment(data[i].createdTs * 1000).twitterLong() +"</span><span class='datakoobai'>@koobai</span><div class='datacont'>"+bbContREG+"</div></li>"
-  }// end for
-  var bbBefore = "<section class='bb-timeline'><ul class='bb-list-ul'>"
-  var bbAfter = "</ul></section>"
-  resultAll = bbBefore + result + bbAfter
-  bbDom.insertAdjacentHTML('beforeend', resultAll);
-  fetchDB()
-  document.querySelector('button.button-load').textContent= '看更多 ...';
-}
+      if(imgUrl){
+        var resImgGrid = ""
+        if(resImgLength !== 1){var resImgGrid = "grid grid-"+resImgLength}
+        bbContREG += '<div class="resimg '+resImgGrid+'">'+imgUrl+'</div></div>'
+      }
+      if(resUrl){
+        bbContREG += '<p class="datasource">'+resUrl+'</p>'
+      }
+    }
+    result += "<li class='bb-list-li' data-module='fade-in-up'><span class='datatime'>"+ moment(data[i].createdTs * 1000).twitterLong() +"</span><span class='datakoobai'>@koobai</span><div class='datacont'>"+bbContREG+"</div></li>"
+}// end for
+var bbBefore = "<section class='bb-timeline'><ul class='bb-list-ul'>"
+var bbAfter = "</ul></section>"
+resultAll = bbBefore + result + bbAfter
+bbDom.insertAdjacentHTML('beforeend', resultAll);
 
-//文章内显示豆瓣条目 https://immmmm.com/post-show-douban-item/
-function fetchDB(){
-  var dbAPI = "https://douban.edui.fun/";
-  var dbA = document.querySelectorAll(".bb-timeline a[href*='douban.com/subject/']:not([rel='noreferrer'])") || '';
-  if(dbA){
-    for(var i=0;i < dbA.length;i++){
-      _this = dbA[i]
-      var dbHref = _this.href
-      var db_reg = /^https\:\/\/(movie|book)\.douban\.com\/subject\/([0-9]+)\/?/;
-      var db_type = dbHref.replace(db_reg, "$1");
-      var db_id = dbHref.replace(db_reg, "$2").toString();
-        if (db_type == 'movie') {
-          var this_item = 'movie' + db_id;
-          var url = dbAPI + "movies/" + db_id ;
-          if (localStorage.getItem(this_item) == null || localStorage.getItem(this_item) == 'undefined') {
-            fetch(url).then(res => res.json()).then( data =>{
-              let fetch_item = 'movies' + data.sid;
-              let fetch_href = "https://movie.douban.com/subject/"+data.sid+"/"
-              localStorage.setItem(fetch_item, JSON.stringify(data));
-              movieShow(fetch_href, fetch_item)
-            });
-          } else {
-            movieShow(dbHref, this_item)
-          }
-        }else if (db_type == 'book') {
-          var this_item = 'book' + db_id;
-          var url = dbAPI + "v2/book/id/" + db_id;
-          if (localStorage.getItem(this_item) == null || localStorage.getItem(this_item) == 'undefined') {
-            fetch(url).then(res => res.json()).then( data =>{
-              let fetch_item = 'book' + data.id;
-              let fetch_href = "https://book.douban.com/subject/"+data.id+"/"
-              localStorage.setItem(fetch_item, JSON.stringify(data));
-              bookShow(fetch_href, fetch_item)
-            });
-          } else {
-            bookShow(dbHref, this_item)
-          }
-        }
-    }// for end
-  }
-}
-function movieShow(fetch_href, fetch_item){
-  var storage = localStorage.getItem(fetch_item);
-  var data = JSON.parse(storage);
-  var db_star = Math.ceil(data.rating);
-  var db_html = "<div class='post-preview'><div class='post-preview--meta'><div class='post-preview--middle'><h4 class='post-preview--title'><a target='_blank' rel='noreferrer' href='" + fetch_href + "'>《" + data.name + "》</a></h4><div class='rating'><div class='rating-star allstar" + db_star + "'></div><div class='rating-average'>" + data.rating + "</div></div><time class='post-preview--date'>导演：" + data.director + " / 类型：" + data.genre + " / " + data.year + "</time><section style='max-height:75px;overflow:hidden;' class='post-preview--excerpt'>" + data.intro.replace(/\s*/g, "") + "</section></div></div><img referrer-policy='no-referrer' loading='lazy' class='post-preview--image' src=" + data.img + "></div>"
-  var db_div = document.createElement("div");
-  var qs_href = ".bb-timeline a[href='"+ fetch_href +"']"
-  var qs_dom = document.querySelector(qs_href)
-  qs_dom.parentNode.replaceChild(db_div, qs_dom);
-  db_div.innerHTML = db_html
-}
-function bookShow(fetch_href, fetch_item) {
-  var storage = localStorage.getItem(fetch_item);
-  var data = JSON.parse(storage);
-  var db_star = Math.ceil(data.rating.average);
-  var db_html = "<div class='post-preview'><div class='post-preview--meta'><div class='post-preview--middle'><h4 class='post-preview--title'><a target='_blank' rel='noreferrer' href='" + fetch_href + "'>《" + data.title + "》</a></h4><div class='rating'><div class='rating-star allstar" + db_star + "'></div><div class='rating-average'>" + data.rating.average + "</div></div><time class='post-preview--date'>作者：" + data.author + " </time><section style='max-height:75px;overflow:hidden;' class='post-preview--excerpt'>" + data.summary.replace(/\s*/g, "") + "</section></div></div><img referrer-policy='no-referrer' loading='lazy' class='post-preview--image' src=" + data.images.medium + "></div>"
-  var db_div = document.createElement("div");
-  var qs_href = ".bb-timeline a[href='"+ fetch_href +"']"
-  var qs_dom = document.querySelector(qs_href)
-  qs_dom.parentNode.replaceChild(db_div, qs_dom);
-  db_div.innerHTML = db_html
+document.querySelector('button.button-load').textContent= '看更多 ...';
 }
