595bfeec3d7e780342126e098f96613fa06c07f4
koobai <50470569+koobai@users.noreply.github.com>
2023-06-07 22:04:09 +0800

    add

--- themes/jingzhe/assets/js/scripts.js
+++ themes/jingzhe/assets/js/scripts.js
@@ -45,38 +45,3 @@ function animateSummaries() {
 }
 
 animateSummaries();
-
-// 首页app模块自动上下滚动
-
-// 获取容器和内容列表
-var indexContainer = document.querySelector('.app-gundong');
-var appList = document.querySelector('.app-list');
-
-// 复制内容列表，使其重复滚动
-appList.innerHTML += appList.innerHTML;
-
-// 动态计算滚动的高度
-var listItemHeight = appList.children[0].offsetHeight;
-var totalHeight = appList.children.length * listItemHeight;
-
-// 设置容器的高度和滚动速度
-indexContainer.style.height = listItemHeight + 'px';
-appList.style.animationDuration = (totalHeight / 25) + 's';
-
-
-// 首页好物随机调用显示
-function getRandomData() {
-  fetch("json/hardware.json")
-    .then(response => response.json())
-    .then(data => {
-      var goods = data.good;
-      var randomIndex = Math.floor(Math.random() * goods.length);
-      var randomGood = goods[randomIndex];
-      document.getElementById("hardware-img").innerHTML = `<img src="${randomGood.image}">`;
-      document.getElementById("hardware-jiage").textContent = `购入价格: RMB ${randomGood.jiage}`;
-      document.getElementById("hardware-title").innerHTML = `<a href="/hardware">${randomGood.title}</a>`;
-      document.getElementById("hardware-note").textContent = randomGood.note;
-    });
-}
-
-getRandomData();
