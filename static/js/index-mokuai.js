0e2044347c8b7fb972475514d10906705013a65d
koobai <50470569+koobai@users.noreply.github.com>
2023-06-14 22:56:41 +0800

    add

--- static/js/index-mokuai.js
+++ static/js/index-mokuai.js
@@ -32,3 +32,24 @@ function getRandomData() {
 }
 
 getRandomData();
+
+
+//首页memos随机头像
+function getRandomImage() {
+  fetch('/suju/about.json')
+    .then(response => response.json())
+    .then(data => {
+      var imagePaths = data.map(item => item.image_path);
+
+      var imgElements = document.querySelectorAll('.memoImage');
+      imgElements.forEach(imgElement => {
+        var randomImagePath = imagePaths[Math.floor(Math.random() * imagePaths.length)];
+        imgElement.setAttribute('src', randomImagePath);
+      });
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+}
+
+window.onload = getRandomImage;
