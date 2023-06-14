de92c7d36f794c1482a32aebc6773bf914653bde
koobai <50470569+koobai@users.noreply.github.com>
2023-06-14 18:10:02 +0800

    add

--- content/_index.md
+++ content/_index.md
@@ -2,8 +2,5 @@
 title: Hello World, Hello 2023
 description: 工作生活记录、博客折腾, 同时分享下平常使用的软件、 观看的影视、喜欢折腾的物品等~~
 ---
-<div class="photo-home img-hide" view-image>
-<script src="/js/photo.js"></script>
-</div>
 {{<memos>}}
 {{< movies 4>}}
--- static/js/photo.js
+++ static/js/photo.js
@@ -11,7 +11,25 @@ const getRandomImages = (arr, num) => {
 
 const insertRandomImages = images => {
   const photoHome = document.querySelector('.photo-home');
-  images.forEach(({ img, imgSmall, title }) => {
+
+  // Handle the first image separately
+  const { img, imgSmall, title } = images[0];
+  const firstPhotoHtml = `
+    <div class="photo-home-top-1">
+      <a href="${img}">
+        <img loading="lazy" decoding="async" src="${imgSmall}" alt="${title}">
+      </a>
+      <span class="photo-home-title-1">${title}</span>
+    </div>
+  `;
+  photoHome.insertAdjacentHTML('beforeend', firstPhotoHtml);
+
+  // Handle the remaining images
+  const remainingImages = images.slice(1);
+  const photoContainer = document.createElement('div'); // 创建一个新的<div>元素
+  photoContainer.classList.add('photo-home-top-2'); // 添加CSS类名
+
+  remainingImages.forEach(({ img, imgSmall, title }) => {
     const photoHtml = `
       <div class="photo-home-top">
         <a href="${img}">
@@ -20,14 +38,16 @@ const insertRandomImages = images => {
         <span class="photo-home-title">${title}</span>
       </div>
     `;
-    photoHome.insertAdjacentHTML('beforeend', photoHtml);
+    photoContainer.insertAdjacentHTML('beforeend', photoHtml);
   });
+
+  photoHome.appendChild(photoContainer); // 将包含所有照片的容器添加到photoHome元素中
 };
 
 fetch('/suju/photo.json')
   .then(response => response.json())
   .then(data => {
-    insertRandomImages(getRandomImages(data, 6));
+    insertRandomImages(getRandomImages(data, 7));
   })
   .catch(error => {
     console.error('无法加载 photo.json 文件:', error);
