c111490c735f1a458102ca3fa4e83e4e3c2225db
koobai <50470569+koobai@users.noreply.github.com>
2023-06-19 17:06:14 +0800

    add

--- static/js/photo.js
+++ static/js/photo.js
@@ -47,7 +47,7 @@ const insertRandomImages = images => {
 fetch('/suju/photo.json')
   .then(response => response.json())
   .then(data => {
-    insertRandomImages(getRandomImages(data, 7));
+    insertRandomImages(getRandomImages(data, 5));
   })
   .catch(error => {
     console.error('无法加载 photo.json 文件:', error);
