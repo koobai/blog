9eee8970bad80ba78ae7849994e4d71cd0d436a1
koobai <50470569+koobai@users.noreply.github.com>
2024-01-09 19:01:54 +0800

    add

--- static/js/memos.js
+++ static/js/memos.js
@@ -369,27 +369,3 @@ function handleDarkModeChange(e) {
   }
 }
 handleDarkModeChange(darkModeMatcher);
-
-//唠叨博文菜单切换js
-
-// 在页面加载时执行一次showContent，根据默认显示的内容设置初始菜单项颜色
-document.addEventListener("DOMContentLoaded", function () {
-  showContent('1');
-});
-function showContent(contentId) {
-  // 隐藏所有内容块
-  var contents = document.querySelectorAll('.index-article-page');
-  contents.forEach(function (content) {
-    content.classList.remove('index-article-memos', 'index-menu-current');
-  });
-  // 显示指定的内容块
-  var selectedContent = document.getElementById('index-article-page' + contentId);
-  selectedContent.classList.add('index-article-memos');
-  // 设置菜单项颜色
-  var menuItems = document.querySelectorAll('.index-menu-tab');
-  menuItems.forEach(function (menuItem) {
-    menuItem.classList.remove('index-menu-current');
-  });
-  var selectedMenuItem = document.querySelector('[onclick="showContent(\'' + contentId + '\')"]');
-  selectedMenuItem.classList.add('index-menu-current');
-}
