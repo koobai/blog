59d769f000f91c38fd485a6cada3287d7316df6f
koobai <mekoobai@gmail.com>
2026-01-31 10:52:30 +0800

    优化

--- static/js/about-photo.js
+++ static/js/about-photo.js
@@ -1,12 +1,17 @@
 //头部背景图随机显示
-var data = [
-  { text: '拍摄于杭州西湖', image: 'https://img.koobai.com/about.webp' },
-  { text: '拍摄于北京长城', image: 'https://img.koobai.com/about-beij.webp' },
-  { text: '拍摄于杭州千岛湖', image: 'https://img.koobai.com/about-qdh.webp' }
-];
+(function() {
+  const data = [
+    { text: '拍摄于杭州西湖', image: 'https://img.koobai.com/about.webp' },
+    { text: '拍摄于北京长城', image: 'https://img.koobai.com/about-beij.webp' },
+    { text: '拍摄于杭州千岛湖', image: 'https://img.koobai.com/about-qdh.webp' }
+  ];
 
-var randomIndex = Math.floor(Math.random() * data.length);
-var randomItem = data[randomIndex];
+  const random = data[Math.floor(Math.random() * data.length)];
+  const img = document.querySelector('.about-img img');
+  const intro = document.querySelector('.about-img-intro');
 
-document.querySelector('.about-img img').src = randomItem.image;
-document.querySelector('.about-img-intro').textContent = randomItem.text;
+  if (img && intro) {
+    img.src = random.image;
+    intro.textContent = random.text;
+  }
+})();
