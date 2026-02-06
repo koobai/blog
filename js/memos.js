52cfb38a6a82603c030dd703918c9e85b89a4632
koobai <koobai@users.noreply.github.com>
2026-02-06 04:28:15 +0000

    deploy: d4535412c66feec1ed02403241cc1b25f027dbad

--- js/memos.js
+++ js/memos.js
@@ -6,7 +6,7 @@
     // 0. 全局常量、正则与样式注入
     // ============================================================
     const REG = {
-        TAG: /#([^#\s!.,;:?"'()]+)(?= )/g,
+        TAG: /(?<=^|\s)#([^#\s!.,;:?"'()]+)/g,
         IMG: /\!\[(.*?)\]\((.*?)\)/g,
         LINK: /\[(.*?)\]\((.*?)\)/g
     };
@@ -19,7 +19,7 @@
     // ============================================================
     const lsPath = window.localStorage?.getItem("memos-access-path");
     const lsToken = window.localStorage?.getItem("memos-access-token");
-    const defaultMemos = 'https://memos.koobai.com/';
+    const defaultMemos = 'https://memos.koobai.com';
 
     // 优化 3: normalizeUrl 内联
     const baseMemos = (lsPath || defaultMemos).replace(/\/?$/, '/');
@@ -635,10 +635,10 @@
                         const key = "memos-oneday";
                         if (!window.localStorage.getItem(key)) {
                             window.localStorage.setItem(key, "open");
-                            cocoMessage.success("已开启回忆，请刷新");
+                            cocoMessage.success("已开启穿越，请刷新");
                         } else {
                             window.localStorage.removeItem(key);
-                            cocoMessage.success("已退出回忆");
+                            cocoMessage.success("已退出穿越");
                             reloadList();
                         }
                     }
@@ -762,7 +762,7 @@
                             <div class="button outline action-btn link-img" title="图片链接"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path fill="currentColor" d="M21 3H3C2 3 1 4 1 5v14c0 1.1.9 2 2 2h18c1 0 2-1 2-2V5c0-1-1-2-2-2m0 15.92c-.02.03-.06.06-.08.08H3V5.08L3.08 5h17.83c.03.02.06.06.08.08v13.84zm-10-3.41L8.5 12.5L5 17h14l-4.5-6z"/></svg></div>
                             <div class="button outline action-btn biao-qing" title="表情"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="10"/><path d="M8 14s1.5 2 4 2 4-2 4-2"/><line x1="9" x2="9.01" y1="9" y2="9"/><line x1="15" x2="15.01" y1="9" y2="9"/></svg></div>
                             <div class="memos-more-ico"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><g fill="none"><path d="M24 0v24H0V0zM12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035c-.01-.004-.019-.001-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427c-.002-.01-.009-.017-.017-.018m.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093c.012.004.023 0 .029-.008l.004-.014l-.034-.614c-.003-.012-.01-.02-.02-.022m-.715.002a.023.023 0 0 0-.027.006l-.006.014l-.034.614c0 .012.007.02.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01z"/><path fill="currentColor" d="M5 10a2 2 0 1 1 0 4a2 2 0 0 1 0-4m7 0a2 2 0 1 1 0 4a2 2 0 0 1 0-4m7 0a2 2 0 1 1 0 4a2 2 0 0 1 0-4m7 0a2 2 0 1 1 0 4a2 2 0 0 1 0-4"/></g></svg>
-                                <div class="memos-xiala"><div class="code-btn">代码</div><div class="private-btn">私有</div><div class="oneday-btn">回忆</div><div class="switchUser-btn">退出</div></div>
+                                <div class="memos-xiala"><div class="code-btn">代码</div><div class="private-btn">私有</div><div class="oneday-btn">穿越</div><div class="switchUser-btn">退出</div></div>
                             </div>
                         </div>
                         <div class="editor-submit d-flex flex-fill justify-content-end">
