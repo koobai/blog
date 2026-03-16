0e97289b5702d155d8d3689d1e536b0013cae822
koobai <mekoobai@gmail.com>
2026-03-16 11:15:52 +0800

    add

--- sync_movies.py
+++ sync_movies.py
@@ -149,7 +149,9 @@ if __name__ == "__main__":
         
         req = urllib.request.Request(douban_api, headers={
             'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 16_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.0 Mobile/15E148 Safari/604.1',
-            'Referer': f'https://m.douban.com/people/{DOUBAN_ID}/interests'
+            'Referer': f'https://m.douban.com/people/{DOUBAN_ID}/interests',
+            'Accept': 'application/json',
+            'Connection': 'keep-alive'
         })
 
         try:
