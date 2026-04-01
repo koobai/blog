09bf7d56f37dda7f9f37cbb44a34e1466813c2f0
koobai <mekoobai@gmail.com>
2026-04-01 14:15:42 +0800

    add

--- static/js/exercise-map.js
+++ static/js/exercise-map.js
@@ -390,7 +390,7 @@ document.addEventListener('DOMContentLoaded', () => {
               </div>
               <div class="panel-share">
                 ${aiComment ? `
-                <button type="button" id="trigger-ai-btn" class="panel-share-btn">
+                <button type="button" id="trigger-ai-btn" class="panel-share-btn" style="color: ${color}">
                   <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path fill="currentColor" d="M19 9l1.25-2.75L23 5l-2.75-1.25L19 1l-1.25 2.75L15 5l2.75 1.25L19 9zm-7.5 .5L9 4L6.5 9.5L1 12l5.5 2.5L9 20l2.5-5.5L17 12l-5.5-2.5zM19 15l-1.25 2.75L15 19l2.75 1.25L19 23l1.25-2.75L23 19l-2.75-1.25L19 15z"/></svg>
                 </button>
                 ` : ''}
